073656d4ce97ddd9f98ee8f2b863c75fefece66d
Vladimir_laptop <vladimir.frolov.99@mail.ru>
2023-09-19 13:22:35 +0300

    delete files from main branch

--- src/main/java/com/example/javaservice/RestServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.javaservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class RestServiceApplication {
-    public static void main ( String[] args ) {
-        SpringApplication.run(RestServiceApplication.class,args);
-    }
-}
